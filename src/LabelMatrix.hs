613e385aa22de537dcd2cdfc73b99173372e7660
2mol <2mol@users.noreply.github.com>
2018-06-08 17:43:22 +0200

    no storable for labels, fromList helper

--- src/LabelMatrix.hs
+++ src/LabelMatrix.hs
@@ -1,12 +1,13 @@
 module LabelMatrix where
 
 -- import Data.Function    ((&))
--- import Foreign.Storable (Storable)
+import Foreign.Storable (Storable)
 
 import           Data.Map              (Map)
 -- import qualified Data.Map              as M
--- import qualified Data.Vector.Storable  as V
-import           Numeric.LinearAlgebra
+import qualified Data.Vector  as V
+-- import qualified Data.Vector.Storable  as VS
+import           Numeric.LinearAlgebra hiding (fromList)
 
 {-
 Matrices with labeled rows and columns.
@@ -22,12 +23,39 @@ elements in the matrix contain only one type.
 data LabelMatrix a r c =
     LabelMatrix
         { matrix :: Matrix a
-        , rows   :: Vector r
-        , cols   :: Vector c
+        , rows   :: V.Vector r
+        , cols   :: V.Vector c
         } deriving (Show)
+
+fromMatrix :: Matrix a -> [r] -> [c] -> LabelMatrix a r c
+fromMatrix = undefined
+
+fromList :: Storable a => [a] -> [r] -> [c] -> LabelMatrix a r c
+fromList content rowLabels columnLabels =
+    let
+        m = length rowLabels
+        n = length columnLabels
+        mat = (m><n) content
+    in
+        LabelMatrix mat (V.fromList rowLabels) (V.fromList columnLabels)
+
+groupBy :: LabelMatrix a r c -> r -> Map a (LabelMatrix a r c)
+groupBy = undefined
 
 groupByRow :: LabelMatrix a r c -> r -> Map a (LabelMatrix a r c)
 groupByRow = undefined
+
+-- groupByVector :: (Ord a, Storable a, Storable b) => Vector a -> Vector b -> Map a (Vector b)
+-- groupByVector groupingVector targetVector =
+--     let
+--         groupedIndices =
+--             indicesMap groupingVector
+
+--         getAtIndices vec indices =
+--             V.ifilter (\i _ -> elem i indices) vec
+--     in
+--         groupedIndices
+--             & M.map (getAtIndices targetVector)
 
 groupByColumn :: LabelMatrix a r c -> c -> Map a (LabelMatrix a r c)
 groupByColumn = undefined
